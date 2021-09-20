88b6ab7d51dfabda1f85150b5c5f981df4a2603e
Alessio Venturi <ale.venturi95@yahoo.it>
2021-09-20 15:50:31 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@
 export const environment = {
   production: true,
   firebaseConfig : {
-    apiKey: "AIzaSyCasgKPTHGYVWE_L_0hr8-JKsHwqCnY8ug",
-    authDomain: "nearme-15871.firebaseapp.com",
-    projectId: "nearme-15871",
-    storageBucket: "nearme-15871.appspot.com",
-    messagingSenderId: "745795755459",
-    appId: "1:745795755459:web:18c0763b81ab79dbb0b176",
-    measurementId: "G-DMSFZXRNLD"
+    apiKey: "xxxxxxxxx",
+    authDomain: "xxxxxxxx",
+    projectId: "xxxxxxxx",
+    storageBucket: "xxxxxxx",
+    messagingSenderId: "xxxxxxx",
+    appId: "xxxxxxx",
+    measurementId: "xxxxxxx"
   }
 };
 
